0c0bd47c89b96760153d75ad93aa83b96a0c6a24
Hassannadeem861 <Professionalwebdeveloper123@gmail.com>
2023-09-05 11:01:03 -0700

    Done

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 
-Swal.fire('Please Enter Your Vote')
+// Swal.fire('Please Enter Your Vote')
+
+let timerInterval
+Swal.fire({
+  title: 'Auto close alert!',
+  html: 'I will close in <b></b> milliseconds.',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading()
+    const b = Swal.getHtmlContainer().querySelector('b')
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft()
+    }, 100)
+  },
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
 
 let poll = {
     question: "What is your favorite programming language?",
